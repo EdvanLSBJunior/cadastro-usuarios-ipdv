974d41b49a82db6bf9fc49cf0b4d83324e195f63
Edvan Brito <edvanbrito.j@hotmail.com>
2025-06-24 14:23:42 -0300

    feat: implements docker compose

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { User } from "../entities/User";
+import { Role } from "../entities/Role";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -8,12 +9,12 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 5432),
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   synchronize: false,
   logging: false,
-  entities: [User],
+  entities: [User, Role],
   migrations: ["src/database/migrations/*.ts"],
 });
